c5e3070be756dcbb15959e566a08e9f4f70e28ed
KaiBlack17 <kailey17black@gmail.com>
2021-05-18 20:24:35 -0600

    dateish

--- lesson4/js/menu.js
+++ lesson4/js/menu.js
@@ -1,3 +1,20 @@
 function togglemenu() {
     document.getElementById("menuitems").classList.toggle("hide");
 }
+
+let daynames = {
+    "Sunday"
+    "Monday"
+    "Tuesday"
+    "Wednesday"
+    "Thursday"
+    "Friday"
+    "Saturday"
+}
+
+let d = new Date();
+let dayname = daynames[d.getDay()];
+
+let fulldate = dayname + ", " + d.getMonth() + " " + d.getDate() +", " + d.getFullYear();
+
+document.getElementById("today").innerHTML = fulldate;
