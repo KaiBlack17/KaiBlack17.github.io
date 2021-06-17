804f9ba3edcc9c51fb3b6bc314223876829541fe
KaiBlack17 <kailey17black@gmail.com>
2021-06-16 18:09:40 -0600

    yay

--- lesson8/js/slider.js
+++ lesson8/js/slider.js
@@ -1,8 +1,4 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("ratenum");
-output.innerHTML = slider.value; // Display the default slider value
 
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
+function adjustRating(rating) {
+  document.getElementById("ratingvalue").innerHTML = rating;
 }
