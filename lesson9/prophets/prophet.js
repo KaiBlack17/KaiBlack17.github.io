ed9ff85d6e66c0f01387223878d36e1f917418f3
KaiBlack17 <kailey17black@gmail.com>
2021-06-17 21:53:36 -0600

    yeet

--- lesson9/prophets/prophet.js
+++ lesson9/prophets/prophet.js
@@ -5,18 +5,31 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    //console.table(jsonObject); FOR TESTING
+    const prophets = jsonObject['prophets'];
+
     for (let i = 0; i < prophets.length; i++ ) {
+      let card = document.createElement('section');
+      let h2 = document.createElement('h2');
+      let p1 = document.createElement('p');
+      let p2 = document.createElement('p');
+      let img = document.createElement('img');
+
+
+      h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+      p1.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+      p2.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+      img.setAttribute('src', prophets[i].imageurl);
+
+      card.appendChild(h2);
+      card.append(p1);
+      card.append(p2);
+      card.append(img);
+
+      document.querySelector('div.cards').appendChild(card);
+    }
+
   });
 
-  const prophets = jsonObject['prophets'];
-  
 
-  let card = document.createElement('section');
-let h2 = document.createElement('h2');
-
-h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-
-card.appendChild(h2);
-
-document.querySelector('div.cards').appendChild(card);
+image.setAttribute('src', prophets[i].imageurl);
