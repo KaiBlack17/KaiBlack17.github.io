409248e97618569593b2237d9e43a48bec158a41
KaiBlack17 <kailey17black@gmail.com>
2021-06-26 16:00:54 -0600

    gah

--- lesson10/cheney/JSON_start/js/scripts.js
+++ lesson10/cheney/JSON_start/js/scripts.js
@@ -1,5 +1,5 @@
 //ADD the key and change units to imperial
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?zip=84065,us&appid=f7dae4438c31f3b6c7876be35f7a832c&units=imperial"
+const apiURL = "//api.openweathermap.org/data/2.5/weather?zip=84065,us&appid=f7dae4438c31f3b6c7876be35f7a832c&units=imperial"
 
 //Go fetch it and then wait for a response.
 fetch(apiURL)
@@ -15,7 +15,7 @@ fetch(apiURL)
     const iconcode = weatherinfo.weather[0].icon;
     console.log(iconcode);
 
-    const icon_path = "http://openweathermap.org/img/w/"+ iconcode +".png";
+    const icon_path = "//openweathermap.org/img/w/"+ iconcode +".png";
     console.log(icon_path);
 
     document.getElementById('weather_icon').src = icon_path;
