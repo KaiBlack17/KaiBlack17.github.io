6d35839d95ead94c9527d684b993e6d82237b1e8
KaiBlack17 <kailey17black@gmail.com>
2021-06-26 15:06:29 -0600

    doom

---
+++ lesson10/apitest/weatherapi.js
@@ -0,0 +1,1 @@
+const requestURL = api.openweathermap.org/data/2.5/weather?id=2172797&appid={API key}
---
+++ lesson10/cheney/JSON_start/js/scripts.js
@@ -0,0 +1,25 @@
+//ADD the key and change units to imperial
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?zip=02111,us&appid=f7dae4438c31f3b6c7876be35f7a832c&units=imperial"
+
+//Go fetch it and then wait for a response.
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((weatherinfo) => {
+    //Once it comes back, display it to the console.
+    console.log(weatherinfo);
+    
+    document.getElementById('place').innerHTML=weatherinfo.name;
+    document.getElementById('currentTemp').innerHTML=weatherinfo.main.temp;
+    document.getElementById('windSpeed').innerHTML=weatherinfo.wind.speed;
+
+    const iconcode = weatherinfo.weather[0].icon;
+    console.log(iconcode);
+
+    const icon_path = "http://openweathermap.org/img/w/"+ iconcode +".png";
+    console.log(icon_path);
+
+    document.getElementById('weather_icon').src = icon_path;
+ }); //end of "then" fat arrow function
+
+
+
