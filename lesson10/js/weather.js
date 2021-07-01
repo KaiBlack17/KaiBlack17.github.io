93ad9d7cad75144e45dffea9829b69fb41a6dcbb
KaiBlack17 <kailey17black@gmail.com>
2021-06-30 22:23:52 -0600

    gah

--- lesson10/js/weather.js
+++ lesson10/js/weather.js
@@ -11,7 +11,7 @@ weekday[5] = "Friday";
 weekday[6] = "Saturday";
 
 
-const apiURL = "http://api.openweathermap.org/data/2.5/forecast?q=Preston,ID,US&appid=f7dae4438c31f3b6c7876be35f7a832c&units=imperial"
+const apiURL = "//api.openweathermap.org/data/2.5/forecast?q=Preston,ID,US&appid=f7dae4438c31f3b6c7876be35f7a832c&units=imperial"
 
 fetch(apiURL)
   .then((response) => response.json())
