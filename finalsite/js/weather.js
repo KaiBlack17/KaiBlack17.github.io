3376c672be51dbc3bd996475e758b0da40c12013
KaiBlack17 <kailey17black@gmail.com>
2021-07-20 22:53:29 -0600

    worrrrk

--- finalsite/js/weather.js
+++ finalsite/js/weather.js
@@ -10,7 +10,7 @@ fetch(requesturl)
     return response.json();
   })
   .then(function (jsonObject) {
-    //console.table(jsonObject); FOR TESTING
+    console.table(jsonObject); //FOR TESTING
     const directory = jsonObject['directory'];
 
     for (let i = 0; i < directory.length; i++ ) {
@@ -57,41 +57,83 @@ fetch(requesturl)
   weekday[5] = "Friday";
   weekday[6] = "Saturday";
 
-  const apiURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=40.521893&lon=-111.939102&appid=f7dae4438c31f3b6c7876be35f7a832c&units=imperial'
-  fetch(apiURL)
-  .then((response) => response.json())
-  .then((weatherinfo) => {
-    console.log(weatherinfo); //FOR TESTING
+  const apiwurl = '//api.openweathermap.org/data/2.5/onecall?lat=40.521893&lon=-111.939102&appid=f7dae4438c31f3b6c7876be35f7a832c&units=imperial'
+  fetch(apiwurl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (jsonObject) {
+      console.table(jsonObject); //FOR TESTING
+      const daily = jsonObject['daily'];
 
-    const todaynumber = mydate.getDay();
-
-    let mylist = weatherinfo.list;
-    let forecastday = todaynumber;
-
-    for (i=0; i< mylist.length; i++) {
-      let time = mylist[i].dt_txt;
-          if (time.includes('21:00:00')) {
-      
-              forecastday += 1;
-              if (forecastday === 7) {forecastday = 0;}
-      
-              let dayname = document.createElement("span")
-              dayname.textContent=weekday[forecastday];
-      
-              let temp = document.createElement("p");
-              temp.textContent = weatherinfo.list[i].daily.temp.day + "\xB0";
-      
-              //let iconcode = weatherinfo.list[i].daily[0].icon;
-              //let iconpath = "http://openweathermap.org/img/w/" + iconcode + ".png";
-              //let theicon = document.createElement("img");
-              //theicon.src=iconpath;
-      
-              let theday = document.createElement("div");
-              theday.appendChild(dayname);
-              theday.appendChild(temp);
-              //theday.appendChild(theicon);
-      
-          document.getElementById('forecast').appendChild(theday);
-          }
+  
+      for (let i = 0; i < daily.length; i++ ) {
+        let sect = document.createElement('section');
+        let h3 = document.createElement('h3');
+        let p1 = document.createElement('p');
+        let p2 = document.createElement('p');
+        let p3 = document.createElement('p');
+  
+        h3.textContent = weekday[i];
+        p1.textContent = daily[i].temp.day +' F';
+        p2.textContent = daily[i].weather[0].description;
+        p3.textContent = 'Humidity: ' + daily[i].humidity;
+        
+        sect.appendChild(h3);
+        sect.appendChild(p1);
+        sect.appendChild(p2);
+        sect.appendChild(p3);
+  
+        document.querySelector('div.weatherforecast').appendChild(sect);
       }
-  });
+  
+    });
+
+
+
+//fetch(apiwurl)
+//.then((response) => response.json())
+//.then((weatherinfo) => {
+//  console.log(weatherinfo);
+
+//  const todaynumber = mydate.getDay();
+
+//  let mylist = weatherinfo.list;
+//  let forecastday = todaynumber;
+
+//  for (i=0; i< mylist.length; i++) {
+    
+//      forecastday += 1;
+//      if (forecastday === 7) {forecastday = 0;}
+    
+//      let dayname = document.createElement("span")
+//      dayname.textContent=weekday[forecastday];
+    
+//      let temp = document.createElement("p");
+//      temp.textContent = weatherinfo.list[i].daily[i].temp.day + "\xB0";
+    
+//      let humid = document.createElement("p");
+//      humid.textContent = weatherinfo.list[i].daily[i].humidity;
+
+//     let desc = document.createElement("p");
+//     desc.textContent = weatherinfo.list[i].daily[i].weather[0].description;
+    
+//      let theday = document.createElement("div");
+//      theday.appendChild(dayname);
+//      theday.appendChild(temp);
+//      theday.appendChild(humid);
+//      theday.appendChild(desc);
+    
+//    document.getElementById('forecast').appendChild(theday);
+        
+//    }
+//});
+
+
+
+
+
+
+
+
+//const apiURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=40.521893&lon=-111.939102&appid=f7dae4438c31f3b6c7876be35f7a832c&units=imperial'
